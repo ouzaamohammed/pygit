aea24db9ccbda53e8049365ef691fa8ee57999f1
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-10-10 18:15:44 +0100

    feat: add hash-object command to get a hashed key based on the content of a file

--- pygit/data.py
+++ pygit/data.py
@@ -1,7 +1,16 @@
 import os
+import hashlib
 
 git_dir = ".pygit"
 
 
 def init():
     os.makedirs(git_dir)
+    os.makedirs(f"{git_dir}/objects")
+
+
+def hash_object(data):
+    oid = hashlib.sha1(data).hexdigest()
+    with open(f"{git_dir}/objects/{oid}", "wb") as out:
+        out.write(data)
+    return oid
