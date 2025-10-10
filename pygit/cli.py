f17c2958dc1939f0b233da9a7f2e5057a84ab5c0
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-10-10 17:32:13 +0100

    feat: add init command to initialize a new repository

--- pygit/cli.py
+++ pygit/cli.py
@@ -1,10 +1,27 @@
 import argparse
+import os
+
+from . import data
 
 
 def main():
+    args = parse_args()
+    args.func(args)
+
+
+def parse_args():
     parser = argparse.ArgumentParser(
         prog="pygit", description="version control system cli"
     )
-    parser.add_argument("init")
-    args = parser.parse_args()
-    print(args.init)
+
+    commands = parser.add_subparsers(dest="command", required=True)
+
+    init_parser = commands.add_parser("init")
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
+def init(args):
+    data.init()
+    print(f"initialized empty pygit directory in {os.getcwd()}/{data.git_dir}")
---
+++ pygit/data.py
@@ -0,0 +1,7 @@
+import os
+
+git_dir = ".pygit"
+
+
+def init():
+    os.makedirs(git_dir)
