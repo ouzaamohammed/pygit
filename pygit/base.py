cad87d14cd9b58e7954e5f1109cfaa71a8c02dd0
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-10-24 01:52:17 +0100

    try different directories when searching for a ref

--- pygit/base.py
+++ pygit/base.py
@@ -1,6 +1,7 @@
 import os
 import itertools
 import operator
+import string
 
 from . import data
 from collections import namedtuple
@@ -131,4 +132,15 @@ def tag(name, oid):
 
 
 def get_oid(name):
-    return data.get_ref(name) or name
+    # name is ref
+    refs_to_try = [f"{name}", f"refs/{name}", f"refs/tags/{name}", f"refs/heads/{name}"]
+    for ref in refs_to_try:
+        if data.get_ref(ref):
+            return data.get_ref(ref)
+
+    # name is SHA1
+    is_hex = all(c in string.hexdigits for c in name)
+    if len(name) == 40 and is_hex:
+        return name
+
+    assert False, f"Unknown name {name}"
