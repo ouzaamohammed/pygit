cdde5631174560295946cd009d115469f7fe89a7
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-10-25 17:43:24 +0100

    write symbolic refs

--- pygit/data.py
+++ pygit/data.py
@@ -39,10 +39,17 @@ ref_value = namedtuple("ref_value", ["symbolic", "value"])
 def update_ref(ref, value, deref=True):
     assert not value.symbolic
     ref = _get_ref_internal(ref, deref)[0]
+
+    assert value.value
+    if value.symbolic:
+        value = f"ref: {value.value}"
+    else:
+        value = value.value
+
     ref_path = f"{git_dir}/{ref}"
     os.makedirs(os.path.dirname(ref_path), exist_ok=True)
     with open(ref_path, "w") as f:
-        f.write(value.value)
+        f.write(value)
 
 
 def get_ref(ref, deref=True):
